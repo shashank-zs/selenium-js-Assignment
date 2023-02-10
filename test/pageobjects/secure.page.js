5c8d21bb6c06f02b448a3595c38d8c275b18291c
Shashank's Mac <raramuri@Raramuris-MacBook-Pro-2.local>
2023-02-10 11:57:59 +0530

    automation for assignment 2

---
+++ test/pageobjects/secure.page.js
@@ -0,0 +1,21 @@
+
+
+// import Page from './page.js';
+
+// /**
+//  * sub page containing specific selectors and methods for a specific page
+//  */
+// class SecurePage extends Page {
+//     /**
+//      * define selectors using getter methods
+//      */
+//     get closeAdd () {
+//         return $('//span[@class="ic_circularclose_grey"]');
+//     }
+//     get logggg () {
+//         return $('//li[@data-cy="account"]');
+//     }
+    
+// }
+
+// export default new SecurePage();
