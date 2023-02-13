aa90abf0de53b09137acafc5d79888ba692b715f
Shashank's Mac <raramuri@Raramuris-MacBook-Pro-4.local>
2023-02-13 17:56:13 +0530

    assignment 1 done

--- wdio.conf.js
+++ wdio.conf.js
@@ -23,7 +23,7 @@ export const config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/**/*.js'
+        './test/specs/**/*.cjs'
     ],
     // Patterns to exclude.
     exclude: [
